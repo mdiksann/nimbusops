ebc87a121f1cb40d8a745b88347334e032673fc7
mdiksann <mdiksann@gmail.com>
2026-09-19 12:16:55 +0700

    fix(oidc): update trust policy subject claim

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -28,7 +28,10 @@ data "aws_iam_policy_document" "github_actions_trust" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repository}:ref:refs/heads/main"]
+      values = [
+        "repo:${var.github_repository}:*",
+        "repo:${split("/", var.github_repository)[0]}*/*${split("/", var.github_repository)[1]}*:*"
+      ]
     }
   }
 }
